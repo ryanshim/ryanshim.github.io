af74df6432e55b05b0542b3581918d53fd3ccf71
Ryan <ryan.shim@gmail.com>
2019-02-20 11:12:57 -0800

    added more buttons, jquery anim., edu/exp/prj sections

---
+++ js/main_anim.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+  var nav_height = $("nav").height();
+
+  $("#main-btn").click(function() {
+    var scroll_pos = $("#trgt-main").offset().top - nav_height;
+    $('html, body').animate({
+      scrollTop: scroll_pos
+    }, 600);
+  });
+
+  $("#edu-btn").click(function() {
+    var scroll_pos = $("#trgt-edu").offset().top - nav_height;
+    $('html, body').animate({
+      scrollTop: scroll_pos
+    }, 600);
+  });
+
+  $("#exp-btn").click(function() {
+    var scroll_pos = $("#trgt-exp").offset().top - nav_height;
+    $('html, body').animate({
+      scrollTop: scroll_pos 
+    }, 600);
+  });
+
+  $("#prj-btn").click(function() {
+    var scroll_pos = $("#trgt-prj").offset().top - nav_height;
+    $('html, body').animate({
+      scrollTop: scroll_pos
+    }, 600);
+  });
+
+  $("#contact-btn").click(function() {
+    var scroll_pos = $("#trgt-contact").offset.top - nav_height;
+    $('html, body').animate({
+      scrollTop: scroll_pos
+    }, 600);
+  });
+});
