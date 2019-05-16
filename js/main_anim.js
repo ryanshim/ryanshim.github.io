352ca127ca6bdf7e09a401de5f70b58155f013d4
Ryan <ryan.shim@gmail.com>
2019-05-16 05:34:51 -0700

    refactored jquery file

--- js/main_anim.js
+++ js/main_anim.js
@@ -1,50 +1,13 @@
 $(document).ready(function() {
   var nav_height = $("nav").height();
-
-
-  $("#main-btn").click(function() {
-    var scroll_pos = $("#trgt-main").offset().top - nav_height;
-    $('html, body').animate({
-      scrollTop: scroll_pos
-    }, 600);
-  });
-
-  $("#edu-btn").click(function() {scroll("target-edu", nav_height);});
-
-  /*
-  $("#edu-btn").click(function() {
-    var scroll_pos = $("#trgt-edu").offset().top - nav_height;
-    $('html, body').animate({
-      scrollTop: scroll_pos
-    }, 600);
-  });
-  */
-
-  $("#exp-btn").click(function() {
-    var scroll_pos = $("#trgt-exp").offset().top - nav_height;
-    $('html, body').animate({
-      scrollTop: scroll_pos 
-    }, 600);
-  });
-
-  $("#prj-btn").click(function() {
-    var scroll_pos = $("#trgt-prj").offset().top - nav_height;
-    $('html, body').animate({
-      scrollTop: scroll_pos
-    }, 600);
-  });
-
-  $("#contact-btn").click(function() {
-    var scroll_pos = $("#trgt-contact").offset.top - nav_height;
-    $('html, body').animate({
-      scrollTop: scroll_pos
-    }, 600);
-  });
+  $("#main-btn").click(function() {scroll("#trgt-main", nav_height);});
+  $("#edu-btn").click(function() {scroll("#trgt-edu", nav_height);});
+  $("#exp-btn").click(function() {scroll("#trgt-exp", nav_height);});
+  $("#prj-btn").click(function() {scroll("#trgt-prj", nav_height);});
+  $("#contact-btn").click(function() {scroll("#trgt-contact", nav_height);});
 });
 
 function scroll(target, nheight) {
-  console.log(target);
-  console.log(nheight);
   var scroll_pos = $(target).offset().top - nheight;
   $('html, body').animate({
     scrollTop: scroll_pos
